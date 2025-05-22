27bf8acba520bfa0736dc7da2c9fff4bff9d8d2f
CJSadowitz <sadowitzc@gmail.com>
2025-05-21 20:09:28 -0400

    toggle display for create account section

--- scripts/login/display_prompts.js
+++ scripts/login/display_prompts.js
@@ -11,6 +11,7 @@ function toggle_login_page() {
     var login_page = document.getElementById("account_login");
     toggle_section(login_page);
     toggle_login_section();
+    toggle_no_acount_section();
 }
 
 function toggle_login_section() {
@@ -20,6 +21,12 @@ function toggle_login_section() {
 
 function toggle_create_account_section() {
     toggle_login_section();
+    toggle_no_acount_section();
     var create_account_section = document.getElementById("account_creation_content");
     toggle_section(create_account_section);
+}
+
+function toggle_no_acount_section() {
+    var no_account_section = document.getElementById("no_account_content");
+    toggle_section(no_account_section);
 }
