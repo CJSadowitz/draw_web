66e5b325f42c6b4738d48434309bfa99c778e032
CJSadowitz <sadowitzc@gmail.com>
2025-05-23 09:55:03 -0400

    mvp login and create account with functioning backend

--- scripts/login/login.js
+++ scripts/login/login.js
@@ -14,6 +14,18 @@ async function login(post_request) {
     console.log(response)
 }
 
-function create_account() {
+async function create_account(post_request) {
+    var username = document.getElementById("ca_username").value;
+    var password = document.getElementById("ca_password").value;
+    var display_name = document.getElementById("ca_display_name").value;
 
+    var message = {
+        type: "create_account",
+        username: username,
+        password: password,
+        display_name: display_name
+    };
+    var response = await post_request(message);
+
+    console.log(response)
 }
